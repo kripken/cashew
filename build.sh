aa4bcbe9f2d93902d9805d7963e7bb938e974dae
Alon Zakai <alonzakai@gmail.com>
2014-11-25 13:29:39 -0800

    test harness

--- build.sh
+++ build.sh
@@ -1,4 +1,2 @@
 ~/Dev/fastcomp/mergebuild/Release+Asserts/bin/clang++ -std=c++11 istring.cpp parser.cpp simple_ast.cpp test.cpp -g
-./a.out &> o
-diff -U5 o text.txt
 
--- test.cpp
+++ test.cpp
@@ -1,10 +1,21 @@
 
 #include "simple_ast.h"
 
-int main() {
+int main(int argc, char **argv) {
+  // Read input file
+  FILE *f = fopen(argv[1], "r");
+  assert(f);
+  fseek(f, 0, SEEK_END);
+  int size = ftell(f);
+  char *src = new char[size+1];
+  rewind(f);
+  int num = fread(src, 1, size, f);
+  assert(num == size);
+  fclose(f);
+  src[size] = 0;
+
   cashew::Parser<Ref, ValueBuilder> builder;
-  char input[] = "print('hello world')";
-  Ref ast = builder.parseToplevel(input);
+  Ref ast = builder.parseToplevel(src);
   std::cout << "output:\n";
   ast->stringify(std::cout, true);
   std::cout << "\n";
