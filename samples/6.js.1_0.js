10f479bd5a754d72f230281e3c1838a2c79735ae
Alon Zakai <alonzakai@gmail.com>
2014-12-01 21:11:44 -0800

    add testcase outputs

---
+++ samples/6.js.1_0.js
@@ -0,0 +1,23 @@
+function func() {
+print(10);
+}
+
+function another(x, y) {
+func();
+ return;
+}
+
+function varr() {
+var x;
+ var a = 5, b, c = func(a, b), d;
+ return 20;
+}
+
+function sub() {
+x[10]
+ x[(y) + (20)]
+ x[30]();
+ x[40] = 50;
+ a
+}
+
