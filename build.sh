99d5c8c5222fa269166b4b275cdaf96592df41a0
Alon Zakai <alonzakai@gmail.com>
2014-12-02 16:46:09 -0800

    add ast traversals

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-~/Dev/fastcomp/mergebuild/Release+Asserts/bin/clang++ -std=c++11 istring.cpp parser.cpp simple_ast.cpp test.cpp -g
+~/Dev/fastcomp/build/Release/bin/clang++ -std=c++11 istring.cpp parser.cpp simple_ast.cpp test.cpp -g
 
